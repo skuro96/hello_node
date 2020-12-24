70ab4d786e6f12c04d89b479cafc66cc24041e19
Sho Kurosu <skurosu@c1r3s7.42tokyo.jp>
2020-12-24 15:07:10 +0900

    update ex04

--- ex04/asyncio.js
+++ ex04/asyncio.js
@@ -1,20 +1,12 @@
 const fs = require('fs');
 
-try
-{
-	fs.readFile(process.argv[2], 'utf8', (err, content) =>
-	{
-		if (err)
-			return ;
+if (process.argv.length < 3)
+	return ;
 
-		var cnt = 0
-		for (var i = 0; i < content.length; i++)
-		{
-			if (content[i] === '\n')
-				cnt++;
-		}
-		console.log(cnt);
-	});
-}
-catch
-{}
+fs.readFile(process.argv[2], (err, content) => {
+	if (err)
+		return ;
+	
+	let lines = content.toString().split('\n');
+	console.log(lines.length - 1);
+});
