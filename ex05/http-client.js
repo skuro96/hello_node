5e1b39979f12e2571fd22f3791c4c977c7ed52d2
Sho Kurosu <skurosu@c1r7s9.42tokyo.jp>
2020-12-29 16:10:32 +0900

    update ex05

--- ex05/http-client.js
+++ ex05/http-client.js
@@ -6,7 +6,13 @@ try
 {
 	http.get(process.argv[2], res => {
 		let html = '';
-		res.on('data', line => html += line);
+		res.on('error', (e) => {
+			console.log(e.message);
+			return ;
+		})
+		res.on('data', (line) => {
+			html += line;
+		});
 		res.on('end', () => {
 			console.log(html.replace(/\n/g, ''));
 		});
