8dbc746a39af35e648539c281e2086f9411862fb
Sho Kurosu <xsho0212@outlook.com>
2020-12-30 01:05:22 +0900

    fin

---
+++ ex01/vars.js
@@ -0,0 +1,6 @@
+console.log('42' + ' is a ' + typeof('42') + '.');
+console.log(42 + ' is a ' + typeof(42) + '.');
+console.log([42] + ' is an ' + typeof([42]) + '.');
+console.log({} + ' is an ' + typeof({}) + '.');
+console.log(true + ' is a ' + typeof(true) + '.');
+console.log(undefined + ' is an ' + typeof(undefined) + '.');
